418b880392efcb15a5aa6f8aa30afb5f6a9b7206
jesse.claven@gmail.com <jesse.claven@gmail.com>
2017-07-06 00:46:02 +0200

    Remove old agent references.

--- AutoVolumeTests/Tests.swift
+++ AutoVolumeTests/Tests.swift
@@ -1,29 +1,28 @@
 //
-//  agent_macOSTests.swift
-//  agent-macOSTests
+//  Tests.swift
+//  AutoVolumeTests
 //
 //  Created by Jesse Claven on 17/12/16.
 //  Copyright © 2016 Jesse Claven. All rights reserved.
 //
 
 import XCTest
-@testable import agent_macOS
+@testable import AutoVolume
 
-class agent_macOSTests: XCTestCase {
+class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+      AutoVolume.AppDelegate
     }
     
     func testPerformanceExample() {
